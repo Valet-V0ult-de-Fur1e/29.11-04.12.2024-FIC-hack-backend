1c5a028e51546253a7802f7fb91c712b6e876456
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-11-28 01:18:19 +0300

    Update main.py

--- router/main.py
+++ router/main.py
@@ -9,5 +9,10 @@ def hello(name: str = ""):
     return {"message": f"Hello {name}"}
 
 
+@app.get("/")
+def root():
+    return {"messege": "have some fun"}
+
+
 if __name__ == "__main__":
    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
