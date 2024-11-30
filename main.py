5b0925aa4d2112bd5c930e666d58f9b37b2b92e7
OptonGroup <opton.group@outlook.com>
2024-12-01 02:03:35 +0300

    change port to 3000

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ app.include_router(targets_router, prefix="/targets")
 app.include_router(currency_router, prefix="/currency", tags=["Currency"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
